c4e5da1de465d0af93b7ab2dbbff5f1b6e68b550
dangphuongthao0303 <dangphuongthao03032003@gmail.com>
2025-02-14 12:02:15 +0700

    update Product list

---
+++ src/java/controller/ProductController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+import entity.PaginatedResponse;
+import entity.Product;
+import model.ProductRepository;
+import service.ProductService;
+
+public class ProductController extends HttpServlet {
+
+    private final ProductService productService = new ProductService();
+    private final ProductRepository productRepository = new ProductRepository();
+
+    private String convertToJson(Object object) {
+        StringBuilder json = new StringBuilder("{");
+        if (object instanceof PaginatedResponse) {
+            PaginatedResponse<?> response = (PaginatedResponse<?>) object;
+            json.append("\"total\":").append(response.getTotal()).append(",");
+            json.append("\"data\":[");
+            for (Object item : response.getData()) {
+                json.append(item.toString()).append(",");
+            }
+            if (!response.getData().isEmpty()) {
+                json.deleteCharAt(json.length() - 1);
+            }
+            json.append("]");
+        }
+        json.append("}");
+        return json.toString();
+    }
+}
